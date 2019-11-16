c5ba62ef76b03c36319966c5d216bc03a589a67e
unknown <galan1451@gmail.com>
2019-11-16 12:58:14 -0600

    scalable

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,4 +3,8 @@ $(document).ready(function(){
 
     $('.sidenav').sidenav();
 
+    var scale = 'scale(1)';
+document.body.style.webkitTransform =  scale;    // Chrome, Opera, Safari
+ document.body.style.msTransform =   scale;       // IE 9
+ document.body.style.transform = scale; 
   });
